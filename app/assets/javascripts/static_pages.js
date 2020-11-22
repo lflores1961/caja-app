8bdc69b72f6a6dfb53eb65622f383ed832e82e2f
Fernando Flores <fernando.flores1961@gmail.com>
2020-11-21 20:35:30 -0600

    Clean styles for sidebar menu to final state and working JQuery accordeon effect

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  alert("Si jala");
+  $('.menu li:has(ul)').click(function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('activado')) {
+      $(this).removeClass('activado');
+      $(this).children('ul').slideUp();
+    } else {
+      $('.menu li ul').slideUp();
+      $('.menu li').removeClass('activado');
+      $(this).addClass('activado');
+      $(this).children('ul').slideDown();
+    }
+  });
 });
